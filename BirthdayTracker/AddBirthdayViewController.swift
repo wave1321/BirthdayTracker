a1b210faea834ec6487f7e22a5121709bf470fd3
wave1321 <snowbovin@gmail.com>
2022-09-19 00:49:29 +0300

    page 177

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -16,8 +16,28 @@ class AddBirthdayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
     }
-
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let birthdate = birthdatePicker.date
+        let newBirthday = Birthday(firstName: firstName, lastName: lastName,
+         birthdate: birthdate)
+        
+        print("Создана запись о дне рождения!")
+        print("Имя: \(newBirthday.firstName)")
+        print("Фамилия: \(newBirthday.lastName)")
+        print("День рождения: \(newBirthday.birthdate)")
+        
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
