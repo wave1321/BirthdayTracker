0aa5d0487efc95b33afa9edee7af991342041da1
wave1321 <snowbovin@gmail.com>
2022-09-20 03:50:44 +0300

    page 218

--- BirthdayTracker/BirthdaysTableViewController.swift
+++ BirthdayTracker/BirthdaysTableViewController.swift
@@ -72,25 +72,35 @@ class BirthdaysTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        
+        if birthdays.count > indexPath.row {
+        let birthday = birthdays[indexPath.row]
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        context.delete(birthday)
+            
+        do {
+            try context.save()
+        } catch let error {
+            print("Не удалось сохранить из-за ошибки \(error).")
+        }
+            
+        birthdays.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
